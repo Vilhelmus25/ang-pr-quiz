81f042e88461f0155d2435e0e5dcade0c3b724eb
Vilhelmus25 <vorvie@gmail.com>
2021-03-30 09:05:45 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { QuizEditorComponent } from './components/quiz-editor/quiz-editor.component';
+import { QuestionEditorComponent } from './components/question-editor/question-editor.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    QuizEditorComponent,
+    QuestionEditorComponent,
+    QuizComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
